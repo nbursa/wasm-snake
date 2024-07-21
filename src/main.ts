0e9645756fdc13e2f72df9613c37308092a32957
Nenad <nbursa@gmail.com>
2024-07-21 17:36:27 +0200

    Cleanup

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,6 @@ async function run() {
     const ctx = canvas.getContext("2d")!;
     const snake = Snake.new(canvas.width / 20, canvas.height / 20);
 
-    console.log('Snake initialized:', snake.render());
-
     document.addEventListener('keydown', (event: KeyboardEvent) => {
         switch (event.key) {
             case 'ArrowUp':
@@ -31,18 +29,17 @@ async function run() {
                 }
                 break;
         }
-        console.log('Direction changed:', snake.get_direction());
     });
 
     function draw() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         const snakeBody = snake.render().split("), (");
-        console.log('Drawing snake body:', snakeBody);
+
         snakeBody.forEach((part: string) => {
             const cleanedPart = part.replace(/[()]/g, '').split(', ');
             const x = parseInt(cleanedPart[0]);
             const y = parseInt(cleanedPart[1]);
-            console.log(`Drawing part at (${x}, ${y})`);
+
             if (!isNaN(x) && !isNaN(y)) {
                 ctx.fillStyle = "lime";
                 ctx.fillRect(x * 20, y * 20, 20, 20);
@@ -54,7 +51,6 @@ async function run() {
 
     function update() {
         snake.update();
-        console.log('Snake updated:', snake.render());
 
         if (snake.check_collision()) {
             console.log('Game Over');
